642783aa2dbca41c8b46d051ad422e7742700314
Heojh <jihaeng1989@gmial.com>
2021-11-21 17:17:08 +0900

    react-native init

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet, TextInput} from 'react-native';
+
+// branch status : main > dev
+function App() {
+  return (
+    <View>
+      <Text>Hello world</Text>
+    </View>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,12 @@
+How to Build.
+
+- with yarn
+  $yarn install
+  $yarn android
+
+- with npm
+  $npm install
+  $npm run android
+
+- or (after install)
+  $react-native run-android
